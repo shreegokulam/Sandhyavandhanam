7f0029c928bb655cae322bc7f11aecee8d7880c1
shreegokulam <shreegokulam@gmail.com>
2026-05-06 21:48:46 +0400

    Update AuthPage.js

--- daily-tracker/src/components/AuthPage.js
+++ daily-tracker/src/components/AuthPage.js
@@ -41,7 +41,7 @@ export default function AuthPage() {
         src="/logo.png" 
         alt="Logo" 
         className="auth-logo" 
-        style={{ maxWidth: '150px', marginBottom: '200px' }} 
+        
       />
           <div className="hero-badge"><span />Daily Sandhyavandhanam Tracker</div>
           <h1>Build habits.<br /><em>Track</em> your<br />daily flow.</h1>
